066fef9b8a1dd5ee0c3bc62770209b9f129318ff
dweb1 <david.p.weber@gmail.com>
2017-07-06 22:46:43 -0400

    Solved Lit Fuse problem

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -4,15 +4,29 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue) {
-  document.getElementById('numeric-display').textContent = timerValue;
+  // document.getElementById('numeric-display').textContent = timerValue;
+  var numericDisplay = document.getElementById('numeric-display');
+    numericDisplay.textContent = timerValue;
+  if (timerValue <= 10) {
+      numericDisplay.style.color = 'red';
+      var newSize = (1.5 * Math.pow(1.05, 11 - timerValue)).toFixed(2);
+      numericDisplay.style.fontSize = newSize + 'em';
+    }
   },
+
+  
   drawProgressBars: function(timerValue){
     var timeElapsed = 100 - timerValue;
     document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
   },
+  
   drawLitFuses: function(timerValue){
-    // Your Code Here
+    var timeElapsed = timerValue;
+    var timeRemaining = 100 - timerValue;
+    document.getElementsByClassName('unburnt')[0].style.width = (timeElapsed - 2) + '%';
+    document.getElementsByClassName('burnt')[0].style.width = (timeRemaining) + '%';
   },
+  
   drawCrawlers: function(timerValue){
     // Your Code Here
   }

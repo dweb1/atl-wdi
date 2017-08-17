c2c8f7c471bfa3a79d51ef4d693b2e56f46a7509
dweb1 <david.p.weber@gmail.com>
2017-08-17 17:51:00 -0400

    Commit: NFL - queried some NFL stuff

---
+++ homework/sql/nfl/nfl.sql
@@ -0,0 +1,7 @@
+1. SELECT name FROM teams;
+2. SELECT name, stadium, head_coach FROM teams WHERE conference = 'NFC';
+3. SELECT head_coach FROM teams WHERE division = 'South' AND conference = 'AFC';
+4. SELECT count(*) FROM players;
+5. 
+6.
+

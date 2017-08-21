595baa4895345417da718698c0bb91a2f229f106
dweb1 <david.p.weber@gmail.com>
2017-08-21 17:41:40 -0400

    5. input/output skills

--- homework/ruby/ruby_skills.rb
+++ homework/ruby/ruby_skills.rb
@@ -18,11 +18,22 @@
 # puts d
 
 # Challenge 4
-christine = "Hi, I'm Christine"
-likesTo = "long walks on the beach, kinda."
-tims = christine + " and I like " + likesTo
-puts tims
-g = "2"
-gToNumber = 2
-puts g + gToNumber
-puts g.to_i + gToNumber
+# christine = "Hi, I'm Christine"
+# likesTo = "long walks on the beach, kinda."
+# tims = christine + " and I like " + likesTo
+# puts tims
+# g = "2"
+# gToNumber = 2
+# puts g + gToNumber
+# puts g.to_i + gToNumber
+
+# Challenge 5
+# puts "Gimme an integer"
+# input = gets.chomp.to_i
+# puts input * 2
+
+puts "Gimme your first name"
+first_name = gets.chomp
+puts "What do you like to do?"
+activity = gets.chomp
+puts first_name + " likes to " + activity

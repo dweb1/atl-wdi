b2a941e364581128012286add51e299c000b9534
dweb1 <david.p.weber@gmail.com>
2017-08-21 17:25:18 -0400

    3. integer skills

--- homework/ruby/ruby_skills.rb
+++ homework/ruby/ruby_skills.rb
@@ -5,6 +5,14 @@
 # puts a != b
 
 #Challenge 2
-a = "Nil"
-puts "#{a} was here"
-puts a.class
+# a = "Nil"
+# puts "#{a} was here"
+# puts a.classg
+
+#Challenge 3
+d = 4
+puts d
+e = 4.5
+puts e
+d = e
+puts d
